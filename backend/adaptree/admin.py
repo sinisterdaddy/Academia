7794bb2a30bb935f16c50d505f58fa5f52f954f1
Radha Krishna <krishna.garg010604@gmail.com>
2024-06-28 22:52:18 +0530

    upd

--- backend/adaptree/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AdapTree
-
-class adaptreeAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', 'major', 'interests')
-
-# Register your models here.
-
-admin.site.register(AdapTree, adaptreeAdmin)
